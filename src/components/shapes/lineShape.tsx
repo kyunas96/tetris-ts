34ab48e55a52153855b6f0df6987e8d3a92ea5b2
Kevin Yunas <kevinyunas@icloud.com>
2021-09-10 16:20:21 -0700

    Added transform methods to abstract shape class

--- src/components/shapes/lineShape.tsx
+++ src/components/shapes/lineShape.tsx
@@ -14,11 +14,17 @@ const LineOrientations = [
 
 class Line extends Shape{
   orientations: Array<ShapeOrientation>
+  orientation: ShapeOrientation;
+  currentOrientationIdx: number
 
   constructor(){
     super();
     this.orientations = LineOrientations;
+    this.currentOrientationIdx = 0;
+    this.orientation = this.orientations[this.currentOrientationIdx];
   }
+
+  
 }
 
 export default Line;
--- src/components/shapes/shape.tsx
+++ src/components/shapes/shape.tsx
@@ -2,8 +2,27 @@ import ShapeOrientation from '../types/shapeOrientation';
 
 abstract class Shape {
   abstract orientations: Array<ShapeOrientation>
+  abstract orientation: ShapeOrientation
+  abstract currentOrientationIdx: number
   // these functions will allow the arrays representing the shapes to be
   // transformed via rotations to update their orientation
+
+
+  rotateRight() {
+    if (this.currentOrientationIdx === this.orientations.length) {
+      this.currentOrientationIdx = 0;
+    } else {
+      this.currentOrientationIdx++;
+    }
+  }
+
+  rotateLeft() {
+    if (this.currentOrientationIdx === 0) {
+      this.currentOrientationIdx = this.orientations.length - 1;
+    } else {
+      this.currentOrientationIdx--;
+    }
+  }
 }
 
 export default Shape;
